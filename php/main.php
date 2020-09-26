ddc50dfa02865c18a95a3c00a21d12adde1d336a
Nel Rondon <nelucho.nel@gmail.com>
2020-09-26 16:10:51 -0400

    version 2

--- php/main.php
+++ php/main.php
@@ -11,14 +11,13 @@
     $fecha_c = date("d/m/Y");
 
     $sql = "INSERT INTO nelrondon.programador (Nombre, Apellido, Cedula, Correo, Lenguajes, Fecha_de_Creacion) VALUES ('$nombre', '$apellido', '$cedula', '$correo', '$lenguajes', '$fecha_c')";
-    mysqli_query($conexion, $sql);
     
     // Resultado de Operacion y Mensaje
     if(mysqli_query($conexion, $sql)){
         $myJSON->result_opr = "La operacion fue realizada con exito";
-        $myJSON->mensage = mysqli_error($conexion);
+        $myJSON->mensage = "Se agrego un nuevo registro a la base de datos";
     }else{
-        $myJSON->mensage = mysqli_error($conexion);
+        $myJSON->mensage = "Ocurrio un error en la ejecucion de la llamada, no fue agregada informacion a la base de datos.";
         $myJSON->result_opr = "La operacion tuvo un problema en la ejecucion";
     }
 
@@ -31,6 +30,24 @@
         $myJSON->numero_programadores = $fila[$i];
         $i++;
     }
+
+    // Presentacion de Datos
+    $sql = "SELECT * FROM nelrondon.programador";
+    $respuesta = mysqli_query($conexion, $sql);
+
+    echo "<table border='1'>";
+    echo "<tr> <td>Nombre</td> <td>Apellido</td> <td>Cedula</td> <td>Correo</td> <td>Lenguajes</td> <td>Fecha de Creacion</td> </tr>";
+    while ($x = mysqli_fetch_array($respuesta)) {
+        echo "<tr>";
+        echo "<td>".$x["Nombre"]."</td>";
+        echo "<td>".$x["Apellido"]."</td>";
+        echo "<td>".$x["Cedula"]."</td>";
+        echo "<td>".$x["Correo"]."</td>";
+        echo "<td>".$x["Lenguajes"]."</td>";
+        echo "<td>".$x["Fecha_de_Creacion"]."</td>";
+        echo "</tr>";
+    }
+    echo "<table> <br>";
 
     echo json_encode($myJSON);
 
